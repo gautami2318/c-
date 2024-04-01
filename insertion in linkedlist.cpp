d41aeb137089de4b97ab0edd7a75d016414daaa9
gautami2318 <165652806+gautami2318@users.noreply.github.com>
2024-04-01 02:27:45 -0700

    Add files via upload

---
+++ insertion in linkedlist.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+struct Node {
+    int data;
+    Node* next;
+};
+void insertAtBeginning(Node*& head, int data) {
+    Node* newNode = new Node{data, head};
+    head = newNode;
+}
+int main() {
+    Node* head = NULL; 
+    insertAtBeginning(head, 10); 
+    insertAtBeginning(head, 20); 
+    insertAtBeginning(head, 30); 
+    
+    for (Node* curr = head; curr != NULL; curr = curr->next) {
+        std::cout << curr->data << " ";
+    }
+   
+    return 0;
+}
