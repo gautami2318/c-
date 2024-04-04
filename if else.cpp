1fa413cc850c8f4fe03346d88036dfbe17bec47f
gautami2318 <165652806+gautami2318@users.noreply.github.com>
2024-04-04 07:25:45 -0700

    Add files via upload

---
+++ if else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int savings;
+	cin>>savings;
+	if(savings>5000)
+	{
+		cout<<"shopping\n";}
+		else{
+		
+		cout<<"save money\n";
+	}
+	return 0;
+}
